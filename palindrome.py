7a0f7cb735c92f7b0c711ced018dd2f60065e23a
shubham-sharma-45 <shubhamvishwakarma1725@gmail.com>
2025-01-07 02:09:46 +0530

    even odd

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+print("welcome to this program")
+#var1=input("Enter words for check palindrome:-")
+var1=-121
+var1=str
+var1=input("Enter words for check palindrome:-")
+var2=""
+for i in range (len(var1)):
+	for i in var1:
+		var2=i+var2
+
+if var2==var1:
