79aacec2c66544413d0a0afed7f473a6710a1641
shubham-sharma-45 <shubhamvishwakarma1725@gmail.com>
2025-01-10 03:22:57 +0530

    'temprature'

---
+++ temprature.py
@@ -0,0 +1,7 @@
+temp=float(input("enter temperature:-"))
+if temp<20:
+	print("i will have chocolate coffee")
+elif temp<30 and temp>20 :
+	print("i will have tea")
+else :
+	print("i will have cold coffee ")
