269a67dab42790637ba475b16dfe8358620593e5
shubham-sharma-45 <shubhamvishwakarma1725@gmail.com>
2025-01-15 05:54:54 +0530

    list methods

---
+++ list_methods.py
@@ -0,0 +1,32 @@
+#list append
+add = [1,"shubham",3.2,34]
+add.append(342)
+print(add)
+
+#list count
+a = [ 1 ,2 ,2 ,3 ,1,2 ,4 ,55]
+x = a.count(2)
+print(x)
+
+#list extent
+fruits = ['apple', 'banana', 'cherry']
+cars = ['Ford', 'BMW', 'Volvo']
+fruits.extend(cars)
+print(fruits)
+
+#list index 
+fruits = ['apple', 'banana', 'cherry','red']
+x = fruits.index("red")
+print(x)
+
+#list pop 
+a = [ 1 ,2 ,2 ,3 ,1,2 ,4 ,55]
+a.pop()
+print(a)
+
+#list reverse
+a = [ 1 ,2 ,2 ,3 ,1,2 ,4 ,55]
+a.reverse()
+print(a)
+
+
