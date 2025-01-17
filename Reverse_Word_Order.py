769899cd23bacd2df495c01336841298bfefaf3a
shubham-sharma-45 <shubhamvishwakarma1725@gmail.com>
2025-01-18 03:54:42 +0530

    Reverse word order

---
+++ Reverse_Word_Order.py
@@ -0,0 +1,26 @@
+x= "aman shubham"
+y = ""
+for i in range(len(x)-1,-1,-1):
+	y = y + x[i]
+	print(y)
+
+z = ""
+for j in range(len(x)) : 
+	z = x[j] + z
+	print(z)
+
+a = x[::-1]
+print(a)
+	
+	
+	
+	
+
+
+
+'''str1 = " i am  a student "
+add = str1.split()
+print(add)
+
+words = add[-1::-1]
+print(words)'''
