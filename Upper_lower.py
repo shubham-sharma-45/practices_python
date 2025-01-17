9b7206fb5f212da6e1da9379c63d43ba77781410
shubham-sharma-45 <shubhamvishwakarma1725@gmail.com>
2025-01-17 07:30:40 +0530

    first character upper

---
+++ Upper_lower.py
@@ -0,0 +1,14 @@
+'''x = input("Enter name :")
+z = x.title()
+print(z)
+'''
+
+n =input("Enter name :")
+x = n.split()
+print(x)
+list1 = []
+for i in x:
+	a = i.capitalize()
+	list1.append(a)
+print(list1)
+
