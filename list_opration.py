dee58d9940cf4ce4626c3cd29e3f50ac177feec8
shubham-sharma-45 <shubhamvishwakarma1725@gmail.com>
2025-01-17 07:33:50 +0530

    methods of list

---
+++ list_opration.py
@@ -0,0 +1,33 @@
+'''var1 = ["25",2.5,25,25+4j,25]
+print(var1)
+print(type(var1))
+for i in range (len(var1)):
+	print(var1[i])
+	print(type(var1[i]))
+for j in var1:
+	print(j)
+	print(type(j))
+
+var1.append("shubham")
+print(var1)
+
+var1.insert(3,"indore")
+print(var1)
+
+x = var1.index("indore")
+var1[x] = "bhopal"
+print(var1)
+
+x = var1.pop()
+print(x)
+print(var1)
+
+x= var1.remove("25")
+print(var1)
+'''
+'''n = int(input("enter value :"))
+list1 =  []
+for i in n :
+	list1.append()
+	print(list1)
+'''
