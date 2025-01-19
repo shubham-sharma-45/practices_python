d92600f415d0055b712a188be5bd10a756fd8f0a
shubham-sharma-45 <shubhamvishwakarma1725@gmail.com>
2025-01-20 04:48:57 +0530

    remove dublicate

--- List_Remove_Duplicates.py
+++ List_Remove_Duplicates.py
@@ -1,6 +1,16 @@
-
-def unique(a):
+'''def unique(a):
 	myset = set(a)
 	return myset
 a = [2, 4, 5, 6, 7, 7, 8, 10, 38, 23, 23, 8, 2]
-print(unique(a))
+print(unique(a))'''
+
+def remove(a):
+	add = []
+
+	for i in a:
+	    if i not in add:
+	        add.append(i)
+	return add
+a = [1, 2, 2, 3, 4, 4, 5,6,6]
+print(remove(a))
+
